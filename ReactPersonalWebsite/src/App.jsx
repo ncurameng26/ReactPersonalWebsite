88f7f9e632159d90e5f32487c7aa685889219ec9
nathancurameng <nathan.curameng@gmail.com>
2023-11-14 18:15:13 -0500

    Created VITE file and git repo, created components folder

---
+++ ReactPersonalWebsite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> Hello, I'm Nathan </h1>
+      <h1> I'm a full stack web developer </h1>
+      <button> Check out My Work</button>
+
+    </>
+  )
+}
+
+export default App
